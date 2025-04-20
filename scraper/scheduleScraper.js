52d1f6aa207ac4e433e87933696dab680aa57722
Israel Nunes <israelnetonunes@gmail.com>
2025-04-20 20:56:21 -0300

    refactor: timers for retry delay

--- scraper/scheduleScraper.js
+++ scraper/scheduleScraper.js
@@ -4,23 +4,14 @@ import { connectAndSendData } from "./sendData.js";
 const platform = 'X';
 const url = 'https://x.com/elonmusk';
 
-let retryDelay = 5000;
-const maxDelay = 3 * 60 * 1000;
-
 async function scheduledScraping() {
     console.log('Starting to scrape...')
     try {
         const data = await scrapeProfile(url);
-
         const sendData = await connectAndSendData(data, platform);
         console.log(`Data sent to database: ${JSON.stringify(sendData)}`);
-
-        retryDelay = 5000;
     } catch (error) {
         console.error("Scraping process failed:", error);
-
-        // Increase delay after failure (up to maximum)
-        retryDelay = Math.min(retryDelay * 1.5, maxDelay);
     }
 };
 
